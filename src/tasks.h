1f6062a1af2f7649dc2c29df14ac4ee12a409ae1
kitor <github@kitor.pl>
2021-03-15 21:54:47 +0100

    tasks: add new context struct, enable task struct from 200D on EOSR

--- src/tasks.h
+++ src/tasks.h
@@ -26,6 +26,24 @@
 
 #include "dryos.h"
 
+#if defined(CONFIG_200D) || defined (CONFIG_R)
+// SJE - I believe this has changed because ARMv6 introduced RFE,
+// which Digic7 is using to restore context when task switching.
+// See e0274f3a onwards.
+//
+// The context for a task seems to always be stored on the stack. 
+// 
+// Because you have to do RFE last, as it changes PC, and because
+// RFE restores CPSR and PC, it's natural to put everything else
+// before them on the stack, so you can pop then RFE.
+struct context
+{
+        uint32_t r[13]; // off_0x00;
+        uint32_t lr;    // off_0x34;
+        uint32_t pc;    // off_0x38;
+        uint32_t cpsr;  // off_0x3c;
+};
+#else
 struct context
 {
         uint32_t                cpsr;           // off_0x00;
@@ -33,6 +51,7 @@ struct context
         uint32_t                lr;             // off_0x38;
         uint32_t                pc;             // off_0x3C;
 }; // 0x40 bytes
+#endif
 
 struct task
 {
@@ -54,7 +73,7 @@ struct task
         uint32_t            unknown_07; // 0x38, 4
         uint32_t            unknown_08; // 0x3c, 4
         uint32_t        taskId;         // 0x40, 4
-#if defined(CONFIG_200D) // probably all Digic7, maybe 8 and 9?  But only confirmed on 200D
+#if defined(CONFIG_200D) || defined(CONFIG_R) // probably all Digic7, maybe 8 and 9?  But only confirmed on 200D
         uint32_t            unknown_09; // 0x44, 4
 #endif
         uint8_t             unknown_0a; // 0x44 / 0x48, 1
@@ -65,7 +84,7 @@ struct task
         uint8_t         sleepReason;    // 0x49 / 0x4d, 1
         uint8_t             unknown_0d; // 0x4a / 0x4e, 1
         uint8_t             unknown_0e; // 0x4b / 0x4f, 1
-#if defined(CONFIG_200D) // again, probably more broadly applicable but this needs testing
+#if defined(CONFIG_200D) || defined(CONFIG_R) // again, probably more broadly applicable but this needs testing
         uint8_t             cpu_requested; // 0x50, 1 // SJE working theory: which CPU can
                                                       // take the task.  0xff means any.
         uint8_t             cpu_assigned; // 0x51, 1  // Which CPU has taken the task,

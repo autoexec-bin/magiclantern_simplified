0a6ebddd5e82c21be276bddebd95936237a4bac8
kitor <github@kitor.pl>
2021-07-12 21:40:54 +0200

    M50: Enable TSKMON

--- platform/M50.110/features.h
+++ platform/M50.110/features.h
@@ -12,6 +12,11 @@
 // anything yet as they're faked / not yet found
 #define FEATURE_SHOW_FREE_MEMORY
 
+#define CONFIG_TSKMON
+#define FEATURE_SHOW_TASKS
+#define FEATURE_SHOW_CPU_USAGE
+#define FEATURE_SHOW_GUI_EVENTS
+
 #undef CONFIG_CRASH_LOG
 #undef CONFIG_TSKMON
 #undef CONFIG_PROP_REQUEST_CHANGE
